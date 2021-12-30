915886e29097d8f4b21aab88a79249f7fd3f0161
min <ekdmsdlfma@gmail.com>
2021-12-30 21:36:15 +0900

    clear

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.display.set_caption("~~그때 그 발명대회~~")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
-from factory import *
+from assets.setting.factory import *
 sceneFac = SceneFactory()
 itemFac = ItemFactory()
 
