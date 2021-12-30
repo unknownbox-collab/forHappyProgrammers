ac91cd3d66a0e08d6b5c244f29e3c545c89f1497
min <ekdmsdlfma@gmail.com>
2021-12-30 21:36:27 +0900

    :)

---
+++ assets/setting/player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self) -> None:
+        self.character = None
+        self.hp = 0
+        self.defence = 0
+        self.atk = 0
+        self.heal = 0
+        self.heart = 0
+        self.ap = 0
+
+        self.buffEffect = {}
+        self.inventory = []
+        self.money = 0
+    
+    def setting(self, character):
+        self.character = character
+        self.hp = character
